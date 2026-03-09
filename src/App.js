1eedeaa2f55ffe10521e9a8bbfed897628a962f2
jownsu <67357324+jownsu@users.noreply.github.com>
2026-03-09 10:45:36 +0800

    Add leadbox script

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
         <RockPaperScissor />
+        <script src="https://staging3-app.mapblazers.com/widget/leadbox-widget.js" data-bid="7dbada4e7de9fd106830ff74c0c391cab6d33b9e9fc73b8210d6c680b7ccf7" data-api-url="https://staging3-app.mapblazers.com/api" data-mb></script>
     </div>
   );
 }
